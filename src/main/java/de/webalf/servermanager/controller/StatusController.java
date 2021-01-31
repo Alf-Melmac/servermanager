d6fed9f78ff5948597480768185ab2c1b112f824
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-02-01 00:01:17 +0100

    Present ipToEnum mappings via api

--- src/main/java/de/webalf/servermanager/controller/StatusController.java
+++ src/main/java/de/webalf/servermanager/controller/StatusController.java
@@ -1,11 +1,14 @@
 package de.webalf.servermanager.controller;
 
+import de.webalf.servermanager.model.Server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 import static de.webalf.servermanager.controller.Urls.API;
 
@@ -22,5 +25,11 @@ public class StatusController {
 	public ResponseEntity<Void> ping() {
 		log.trace("ping");
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/mappings")
+	public Map<String, String> getMappings() {
+		log.trace("getMappings");
+		return Server.getIpToEnumMap();
 	}
 }
--- src/main/java/de/webalf/servermanager/model/Server.java
+++ src/main/java/de/webalf/servermanager/model/Server.java
@@ -3,6 +3,10 @@ package de.webalf.servermanager.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 import static de.webalf.servermanager.model.Constants.CMD_EXE;
 import static de.webalf.servermanager.model.Constants.EXECUTE_AND_TERMINATE;
 
@@ -13,9 +17,19 @@ import static de.webalf.servermanager.model.Constants.EXECUTE_AND_TERMINATE;
 @Getter
 @AllArgsConstructor
 public enum Server {
-	TEST(new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Test File.lnk\""}, new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL test.bat"}),
-	ARMA(new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Server Autostart.lnk\""}, new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Server Autostop.lnk\""});
+	TEST(new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Test File.lnk\""}, new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL test.bat"}, "test"),
+	ARMA(new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Server Autostart.lnk\""}, new String[]{CMD_EXE, EXECUTE_AND_TERMINATE, "CALL \"Server Autostop.lnk\""}, "185.232.71.213:2302");
 
 	private final String[] startFilePath;
 	private final String[] stopFilePath;
+	private final String ip;
+
+	@Getter
+	private static final Map<String, String> ipToEnumMap = new HashMap<>();
+
+	static {
+		for (Server server : EnumSet.allOf(Server.class)) {
+			ipToEnumMap.put(server.getIp(), server.toString());
+		}
+	}
 }
